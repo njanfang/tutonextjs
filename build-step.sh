cc41d057d2f4938fa3f42c68d91294ddfb627311
njanfang <njanfang@gmail.com>
2024-06-19 03:38:02 +0100

    Update build-step.sh

--- build-step.sh
+++ build-step.sh
@@ -5,6 +5,6 @@
  if [ -n "$container_id" ]; then
  docker cp /var/lib/jenkins/workspace/tutonextjs/. "$container_id":/usr/share/nginx/html
  else
- docker build -t server /var/lib/jenkins/workspace/tutonextjs
- docker run -d -p 9190:81 server
+ docker build -t server1 /var/lib/jenkins/workspace/tutonextjs
+ docker run -d -p 9190:81 server1
  fi
